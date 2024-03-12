acdf8c52d0ec48bbb725dc4d0e2e51124db16320
森 真輝人 <mkt.it.engr@gmail.com>
2024-03-12 22:25:27 +0900

    検索コンポーネントのテストのリファクタ

--- ui-test/src/components/UserSearch.test.tsx
+++ ui-test/src/components/UserSearch.test.tsx
@@ -1,34 +1,57 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { UserSearch } from "./UserSearch";
 import userEvent from "@testing-library/user-event";
 import axios from "axios";
 
+jest.mock("axios");
+//TSの場合、コンパイラにAxiosがモック化されていることを明示明示的に伝える必要がある
+const mockAxios = jest.mocked(axios);
+
 const user = userEvent.setup();
-describe("UserSearchコンポーネントのテスト", () =>
-  // 1. 入力フィールドに値を入力し、検索ボタンをクリックすると適切なAPIリクエストが発生することを確認する
-  it("入力フィールドに値を入力し、検索ボタンをクリックすると適切なAPIリクエストが発生することを確認する", async () => {
-    //axiosのモック化
-    const axiosSpy = jest
-      .spyOn(axios, "get")
-      .mockResolvedValue({ data: { id: 1, name: "test" } });
+describe("UserSearchコンポーネントのテスト", () => {
+  beforeEach(async () => {
+    //axiosのモックを解除
+    mockAxios.get.mockReset();
+  });
 
-    //レンダリング
+  it("入力フォームに入力した内容でAPIリクエストが送信されること", async () => {
+    //レスポンスのモック化
+    const userInfo = {
+      id: 1,
+      name: "test",
+    };
+    const resp = { data: userInfo };
+    mockAxios.get.mockResolvedValue(resp);
+
     render(<UserSearch />);
-
-    //入力フィールドに値を入力
     const input = screen.getByRole("textbox");
-    await user.type(input, "test");
+    await user.type(input, userInfo.name);
+    const button = screen.getByRole("button");
+    await user.click(button);
+    expect(mockAxios.get).toHaveBeenCalledWith(
+      `/api/users?query=${userInfo.name}`
+    );
+  });
 
-    //検索ボタンをクリック
+  it("APIから取得したユーザー情報が画面に表示されること", async () => {
+    //レスポンスのモック化
+    const userInfo = {
+      id: 1,
+      name: "test",
+    };
+    const resp = { data: userInfo };
+    mockAxios.get.mockResolvedValue(resp);
+
+    render(<UserSearch />);
+    const input = screen.getByRole("textbox");
+    await user.type(input, userInfo.name);
     const button = screen.getByRole("button");
     await user.click(button);
 
-    //APIリクエストが発生していることを確認
-    expect(axiosSpy).toHaveBeenCalledWith("/api/users?query=test");
+    await waitFor(() =>
+      expect(screen.getByText(userInfo.name)).toBeInTheDocument()
+    );
+  });
+});
 
-    //testが表示されていることを確認
-    expect(await screen.findByText("test")).toBeInTheDocument();
-
-    //axiosのモックを解除
-    axiosSpy.mockRestore();
-  }));
+//axiosのモックを解除
