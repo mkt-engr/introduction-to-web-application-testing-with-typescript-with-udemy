134c913a33d262cadb1fb1c45bea3a9893d644dc
森 真輝人 <mkt.it.engr@gmail.com>
2024-03-22 18:55:45 +0900

    addExpenseのテスト

--- integration-and-e2e/backend/src/services/expenseService.test.ts
+++ integration-and-e2e/backend/src/services/expenseService.test.ts
@@ -30,4 +30,29 @@ describe("ExpenseService", () => {
       mockGroupService as GroupService
     );
   });
+
+  describe("addExpenseのテスト", () => {
+    it("支出が登録されること", () => {
+      (mockGroupService.getGroupByName as jest.Mock).mockReturnValueOnce(group);
+      expenseService.addExpense(expense);
+      expect(mockExpenseRepository.saveExpense).toHaveBeenCalledWith(expense);
+    });
+
+    it("グループが存在しない場合はエラーがスローされること", () => {
+      (mockGroupService.getGroupByName as jest.Mock).mockReturnValueOnce(
+        undefined
+      );
+      expect(() => expenseService.addExpense(expense)).toThrowError(
+        "グループ： group1 が存在しません"
+      );
+    });
+    it("支払い者がグループに存在しない場合はエラーがスローされること", () => {
+      (mockGroupService.getGroupByName as jest.Mock).mockReturnValueOnce(group);
+
+      const nonMemberExpense = { ...expense, payer: "user3" };
+      expect(() => expenseService.addExpense(nonMemberExpense)).toThrowError(
+        "支払い者がメンバーの中にいません"
+      );
+    });
+  });
 });
